ff4e75ac5fb5c85d40711bea95bcd4b4e5247ef6
lukas142434 <lukas.li@student.tdsb.on.ca>
2024-01-13 17:09:51 -0500

    a

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,8 +4,8 @@ import asl from '../images/asl.png'
 function Home() {
   return (
     <div className='flex justify-center'>
-      <img src={asl} alt='asl' className='scale-50'/>
-      <h1 className='font-ShinGoPro text-white text-xl'>Silyntax</h1>
+      <img src={asl} alt='asl' className='w-[180px] h-[82.25]'/>
+      <h1 className='font-ShinGoPro text text-8xl'>Silyntax</h1>
       
     </div>
   );
