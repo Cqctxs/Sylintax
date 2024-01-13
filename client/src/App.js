e6b88d754d0becf575a894c2ba5c6714cb06b6b8
lukas142434 <lukas.li@student.tdsb.on.ca>
2024-01-13 13:56:17 -0500

    a

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
+import { NavigationMenu } from "../components/ui/navigation-menu";
 
 
 function App() {
   return (
     <div className="bg-neutral-900 min-h-screen">
-n    </div>
+      <div className="flex flex-col items-center justify-center min-h-screen text-white">
+        <h1 className="text-4xl font-bold">Hello, world!</h1>
+        <p className="mt-2">This is a React app.</p>
+      </div>
+      <NavigationMenu/>
+n   </div>
   );
 }
 
