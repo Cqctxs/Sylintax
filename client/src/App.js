3ed13f200fa393f8093331d08a9a2a445094cb15
lukas142434 <lukas.li@student.tdsb.on.ca>
2024-01-13 15:00:18 -0500

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,10 @@
-import { NavigationMenu } from "../components/ui/navigation-menu";
+import { Button } from './components/ui/button'
 
 
 function App() {
   return (
     <div className="bg-neutral-900 min-h-screen">
-      <div className="flex flex-col items-center justify-center min-h-screen text-white">
-        <h1 className="text-4xl font-bold">Hello, world!</h1>
-        <p className="mt-2">This is a React app.</p>
-      </div>
-      <NavigationMenu/>
+      <Button variant="outline">Button</Button>
 n   </div>
   );
 }
