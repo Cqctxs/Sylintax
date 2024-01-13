a4e61e85472b1ab657eba45a902ebb1926ca1f63
lukas142434 <lukas.li@student.tdsb.on.ca>
2024-01-13 15:59:49 -0500

    a

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <h1 style={{ fontFamily: 'AOTFShinGoProBold', src: 'url(src/fonts/AOTFShinGoProBold.woff)' }}>
+        Silentscript
+      </h1>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
