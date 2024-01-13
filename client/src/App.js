26d550385d3bac58ff1d7750942875da4d75532c
lukas142434 <lukas.li@student.tdsb.on.ca>
2024-01-13 13:16:49 -0500

    shadcn ui

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,7 @@
 function App() {
   return (
     <div className="bg-neutral-900 min-h-screen">
-      <h1 className="text-3xl">Hello Worldaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</h1>
-    </div>
+n    </div>
   );
 }
 
