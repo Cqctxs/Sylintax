78062c2e45a17bcd188f7565a2f363dc5762ca87
lukas142434 <lukas.li@student.tdsb.on.ca>
2024-01-13 18:00:39 -0500

    a

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,10 +3,13 @@ import asl from '../images/asl.png'
 
 function Home() {
   return (
-    <div className='flex justify-center'>
-      <img src={asl} alt='asl' className='w-[180px] h-[82.25]'/>
-      <h1 className='font-ShinGoPro text-primary-color text-8xl'>Silyntax</h1>
-      
+    <div className='h-screen flex flex-col justify-center items-center'>
+      <div className='absolute bottom-5 left-0 flex justify-center'>
+        <img src={asl} alt='asl' className='w-[180px] h-[82.25]' />
+        <h1 className='font-ShinGoPro text-primary-color text-8xl'>Silyntax</h1>
+        <p className='font-Poppins text-primary-color text-2xl'>is an online platform that provides a comprehensive and interactive learning experience for American Sign Language (ASL) learners, catering to different types of learners, including college students, language enthusiasts, interpreters, homeschoolers, parents of deaf children, and professionals 1.</p>
+
+      </div>
     </div>
   );
 }
