7d90153a818e0b25dc373077918127572bf8ccf6
Sean <seanzhao2007@gmail.com>
2024-01-13 15:41:23 -0500

    Routes :D

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,44 @@
-import { Button } from './components/ui/button'
+//Components
+import Layout from "./components/Layout";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import RequireAuth from "./components/RequireAuth";
+import Unauthorized from "./components/Unauthorized";
+import Home from "./components/Home";
+import Missing from "./components/Missing";
 
+import PersistLogin from "./components/PersistLogin";
+import { Routes, Route } from "react-router-dom";
+import Lesson from "./components/Lesson";
+
+const ROLES = {
+  User: 2001,
+  Editor: 1984,
+  Admin: 5150,
+};
 
 function App() {
   return (
     <div className="bg-neutral-900 min-h-screen">
-      <Button variant="outline">Button</Button>
-n   </div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="unauthorized" element={<Unauthorized />} />
+
+          <Route element={<PersistLogin />}>
+            <Route element={<RequireAuth allowedRoles={[ROLES.Admin, ROLES.Editor, ROLES.User]} />}>
+              <Route path="lesson" element={<Lesson />} />
+            </Route>
+          </Route>
+
+          {/* Just changes the allowedRoles array to change the allowed roles */}
+
+          <Route path="*" element={<Missing />} />
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './globals.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AuthProvider } from "./context/AuthProvider";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
