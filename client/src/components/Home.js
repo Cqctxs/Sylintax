2326b22994d8a903a573c849a723437207e2d748
lukas142434 <lukas.li@student.tdsb.on.ca>
2024-01-13 17:23:41 -0500

    changes to home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,7 +5,7 @@ function Home() {
   return (
     <div className='flex justify-center'>
       <img src={asl} alt='asl' className='w-[180px] h-[82.25]'/>
-      <h1 className='font-ShinGoPro text text-8xl'>Silyntax</h1>
+      <h1 className='font-ShinGoPro text-primary-color text-8xl'>Silyntax</h1>
       
     </div>
   );
