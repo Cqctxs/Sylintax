ce2fd87df9cbbad61f8029038782a4191a4cfaa4
Sean <seanzhao2007@gmail.com>
2024-01-13 23:36:45 -0500

    made lesson card look cool

--- client/src/components/ui/lessoncard.jsx
+++ client/src/components/ui/lessoncard.jsx
@@ -7,18 +7,22 @@ import {
     CardHeader,
     CardTitle,
 } from './card';
+import { Button } from './button';
 
 const LessonCard = ({ title }) => {
     return (
-        <Card className='p-10 w-1/3'>
-            <CardTitle>{title}</CardTitle>
-            <CardDescription>
+        <Card className='p-10 w-1/3 bg-background-color border-primary-color border-2 text-text-color'>
+            <CardTitle className='font-ShinGoPro mb-5'>{title}</CardTitle>
+            <CardDescription className='text-primary-color'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
             </CardDescription>
+            <CardContent>
+                <Button></Button>
+            </CardContent>
             <CardFooter className='flex justify-center items-center'>
                 <div className='relative w-full pt-5'>
-                    <div className='rounded-full absolute left-0 bg-black w-full h-10'></div>
-                    <div className='rounded-full absolute left-0 bg-green-500 w-10 h-10'></div>
+                    <div className='rounded-full absolute left-0 bg-secondary-color w-full h-10'></div>
+                    <div className='rounded-full absolute left-0 bg-primary-color w-10 h-10'></div>
                 </div>
             </CardFooter>
         </Card>
