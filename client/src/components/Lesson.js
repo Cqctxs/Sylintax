0ef8992a3f80235bc10dbf9638468edba77f4dcf
Sean <seanzhao2007@gmail.com>
2024-01-13 20:49:51 -0500

    trying to add cohere implementation in frontend

--- client/src/components/Lesson.js
+++ client/src/components/Lesson.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import useAuth from 'src/hooks/useAuth';
+import useCohere from '../hooks/useCohere';
 
 function Lesson() {
   const { auth } = useAuth();
+  const response = useCohere("A");
+  const [data, setData] = useState("");
+  // useEffect(() => {
+  //   setData(response);
+  // }, [])
+  console.log(response);
   return (
     <div className="text-text-color">
       {auth?.user} {JSON.stringify(auth.completed)}
+      <br/>
+      {data}
     </div>
   )
 }
