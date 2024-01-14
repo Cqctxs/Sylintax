deed0fb14302579f9ead9905ba33919484b99439
Sean <seanzhao2007@gmail.com>
2024-01-13 23:19:30 -0500

    fixed lesson request

--- server/routes/api/lesson.js
+++ server/routes/api/lesson.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const lessonController = require('../../controllers/api/lessonController');
 
 router.post('/', lessonController.createNewLesson);
-router.get('/', lessonController.getLessonById);
+router.get('/:id', lessonController.getLessonById);
 
 module.exports = router;
