d053534c6b0d9b426061ecf4baa86536c6a7d39f
Felix <felix.zhao2@student.tdsb.on.ca>
2024-01-14 08:17:17 -0500

    commit rain

--- client/src/components/ui/rain.jsx
+++ client/src/components/ui/rain.jsx
@@ -3,15 +3,16 @@ const valid = 'ABCDEFGHIKLMNOPQRSTUVWXY'
 const minSize = 15;
 const maxSize = 50;
 
-const randInRange = (min, max) => Math.floor(Math.random*(max-min)) + min;
+const randInRange = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
-const random = () => valid.charAt(Math.floor(Math.random*valid.length))
+const rand = () => valid.charAt(Math.floor(Math.random()*valid.length))
 
 const genRandStream = () => 
-    new Array(parseInt(randInRange(minSize, maxSize))).fill().map(_ => random)
+    new Array(parseInt(randInRange(minSize, maxSize))).fill().map(_ => rand())
 
 const Rain = () => {
     return <div className=" [writing-mode:vertical-lr] whitespace-nowrap select-none" style={{ textOrientation: 'upright' }}>
+        {console.log(genRandStream())}
         {genRandStream().split('').map(char =>
         <img
             className="inline-block w-[50px] h-[50px]"
