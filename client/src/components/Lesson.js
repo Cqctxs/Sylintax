ac557cc6f6a791a3e95def836bdad4eb7fac589c
Felix <felix.zhao2@student.tdsb.on.ca>
2024-01-14 04:51:19 -0500

    asdfa

--- client/src/components/Lesson.js
+++ client/src/components/Lesson.js
@@ -24,7 +24,32 @@ function Lesson() {
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
     setImgSrc(imageSrc);
+    console.log(imageSrc)
+
+    const formData = new FormData();
+
+    // Make a POST request to the Flask server
+    axios.post('http://localhost:5000/verify', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    })
+    .then(response => console.log(response.data))
+    .catch(error => console.error('Error uploading file', error));
   }, [webcamRef]);
+
+  function dataURItoBlob(dataURI) {
+    const byteString = atob(dataURI.split(',')[1]);
+    const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+    const ab = new ArrayBuffer(byteString.length);
+    const ia = new Uint8Array(ab);
+  
+    for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+  
+    return new Blob([ab], { type: mimeString });
+  }
 
   const retake = () => {
     setImgSrc(null);
@@ -77,7 +102,7 @@ function Lesson() {
             {imgSrc ? (
               <img className="absolute border-2 border-text-color rounded-lg shadow-sm" src={imgSrc} alt="webcam" width={600} height={600} />
             ) : (
-              <Webcam className='absolute border-2 border-text-color rounded-lg shadow-sm' height={600} width={600} ref={webcamRef} mirrored={true} />
+              <Webcam className='absolute border-2 border-text-color rounded-lg shadow-sm' height={600} width={600} ref={webcamRef} mirrored={true} screenshotFormat="image/jpeg" />
             )}
             {imgSrc ? (
               <div></div>
--- python/api.py
+++ python/api.py
@@ -1,21 +1,34 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+import numpy as np
+import cv2
 from inference_classifier import detect_sign
+import base64
 
-app = Flask(__name__)
 PORT = 5000
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
-def create_app(test_config=None ):
-    # create and configure the app
-    app = Flask(__name__)
+@app.route('/verify', methods=['POST'])
+def verify():
+    print(request.files)
+    if 'file' not in request.files:
+        print("No file provided")
+        return jsonify({'error': 'No file provided'}), 400
 
-    @app.route('/verify')
-    def return_prediction(image):
-        return detect_sign(image)
+    file = request.files['file']
     
-    return app
+    img_bytes = file.read()
 
-APP = create_app()
+    # Convert the image bytes to a NumPy array
+    nparr = np.frombuffer(img_bytes, np.uint8)
+
+    # Decode the NumPy array to an OpenCV image
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    
+    print("image recieved!")
+
+    return detect_sign(img)
 
 if __name__ == '__main__':
-    APP.run(debug = True)
+    app.run(debug=True)
