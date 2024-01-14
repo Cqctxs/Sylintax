462b3433596165c635118e2811c1949511129258
Sean <seanzhao2007@gmail.com>
2024-01-13 20:54:14 -0500

    fixed error :D

--- client/src/components/Lesson.js
+++ client/src/components/Lesson.js
@@ -4,12 +4,10 @@ import generate from '../api/cohere';
 
 function Lesson() {
   const { auth } = useAuth();
-  console.log(response);
   return (
     <div className="text-text-color">
       {auth?.user} {JSON.stringify(auth.completed)}
       <br/>
-      {data}
     </div>
   )
 }
