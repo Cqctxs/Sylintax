2a596c942b89b81a8c836d709d89e783bca0e28f
lukas142434 <lukas.li@student.tdsb.on.ca>
2024-01-14 08:22:15 -0500

    a

--- client/src/components/ui/rain.jsx
+++ client/src/components/ui/rain.jsx
@@ -8,7 +8,7 @@ const randInRange = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 const rand = () => valid.charAt(Math.floor(Math.random()*valid.length))
 
 const genRandStream = () => 
-    new Array(parseInt(randInRange(minSize, maxSize))).fill().map(_ => rand())
+    new Array(parseInt(randInRange(minSize, maxSize))).fill().map(_ => rand()).join('')
 
 const Rain = () => {
     return <div className=" [writing-mode:vertical-lr] whitespace-nowrap select-none" style={{ textOrientation: 'upright' }}>
