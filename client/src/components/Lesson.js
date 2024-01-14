cfe34f3981d403eccc9d6a6b5dd296391c487e8f
Sean <seanzhao2007@gmail.com>
2024-01-13 20:52:51 -0500

    added generate

--- client/src/components/Lesson.js
+++ client/src/components/Lesson.js
@@ -1,14 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import useAuth from 'src/hooks/useAuth';
-import useCohere from '../hooks/useCohere';
+import generate from '../api/cohere';
 
 function Lesson() {
   const { auth } = useAuth();
-  const response = useCohere("A");
-  const [data, setData] = useState("");
-  // useEffect(() => {
-  //   setData(response);
-  // }, [])
   console.log(response);
   return (
     <div className="text-text-color">
