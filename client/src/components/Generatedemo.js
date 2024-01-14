d81f373b22a5317a31250a6403ff540dd244e605
Sean <seanzhao2007@gmail.com>
2024-01-14 10:46:54 -0500

    asdf

--- client/src/components/Generatedemo.js
+++ client/src/components/Generatedemo.js
@@ -4,7 +4,7 @@ import generate from 'src/api/cohere';
 function Generatedemo() {
     const [loading, setLoading] = useState(false);
     const [result, setResult] = useState("");
-    const generateResponse = async (letter) => {
+    const fetchData = async (letter) => {
         try {
           setLoading(true);
           const message = await generate(letter);
@@ -16,12 +16,8 @@ function Generatedemo() {
       };
   return (
     <div className="text-text-color">
-        <button onclick={() => {
-            generateResponse("A")
-        }} className="mt-44">Click me</button>
-      {loading ? (<p>Analyzing...</p>) : (
-        <p>{result}</p>
-      )}
+        {loading ? "Analyzing..." : `Result: ${result}`}
+        <button onClick={fetchData}>Fetch Data</button>
     </div>
   )
 }
